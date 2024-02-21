8a0a0587fde064ee153b17d3f09b8dee5b1bda87
Mohammad <moh.hossain98@gmail.com>
2024-02-21 15:12:01 -0500

    starter

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      {/* Stellar                                        Buy now | Learn More */}
+      <nav className="navbar">
+        <h2>Stellar</h2>
+        <div>
+          <a href="#">Buy now</a>
+          <a href="#">Learn More</a>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/data.js
@@ -0,0 +1,33 @@
+const vrHeadset = {
+  name: "VR Experience Pro",
+  brand: "TechVision",
+  price: 299.99,
+  description:
+    "Immerse yourself in stunning virtual reality experiences with the VR Experience Pro headset.",
+  features: [
+    "High-definition display for crisp visuals",
+    "Immersive 360-degree sound",
+    "Adjustable head strap for comfortable wear",
+    "Compatible with a wide range of VR apps and games",
+    "Includes motion tracking sensors for precise movement",
+    "Intel® RealSense™ technology for realistic depth perception",
+    "Nvidia G-Sync support for smooth, tear-free gaming",
+  ],
+  specifications: {
+    displayType: "OLED",
+    resolution: "2560 x 1440 pixels",
+    refreshRate: "90 Hz",
+    fieldOfView: "110 degrees",
+    connectivity: "USB-C, Bluetooth",
+    compatibility: "Works with smartphones and PCs",
+    weight: "350 grams",
+  },
+  images: [
+    "https://static.vecteezy.com/system/resources/previews/035/907/488/non_2x/ai-generated-modern-white-vr-headset-free-png.png",
+    "https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvczg4LXBvbS0xNTU0LW1vY2t1cC5wbmc.png",
+  ],
+  background:
+    "https://img.freepik.com/free-vector/wave-background-abstract-gradient-design_483537-3688.jpg",
+};
+
+export default vrHeadset;
